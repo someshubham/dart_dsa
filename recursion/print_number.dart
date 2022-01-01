49dc4ce4f91c512f45ab2ea5fa7254d4ca836a91
Shubham Jain <someshubham@gmail.com>
2022-01-01 15:15:47 +0530

    Add first two recursion problems

---
+++ recursion/print_number.dart
@@ -0,0 +1,20 @@
+void main() {
+  print("Ascending Order");
+  printAscending(7);
+  print("Descending Order");
+  printDescending(7);
+}
+
+/// Print Number from 1 to N
+void printAscending(int n) {
+  if (n == 0) return;
+  printAscending(n - 1);
+  print(n);
+}
+
+/// Print number from N to 1
+void printDescending(int n) {
+  if (n == 0) return;
+  print(n);
+  printDescending(n - 1);
+}
---
+++ recursion/subset_string.dart
@@ -0,0 +1,18 @@
+void main() {
+  subset("ab", "");
+}
+
+/// Print Subset of a String
+/// if ab is the string then subset of ab ->
+/// {"","a","b","ab"}
+void subset(String input, String output) {
+  if (input.isEmpty) {
+    print(output);
+    return;
+  }
+
+  // Choosing the first letter
+  subset(input.substring(1), output + input[0]);
+  // Not Choosing the first letter
+  subset(input.substring(1), output);
+}
